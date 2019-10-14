29539f62963f2d8d99c3263eaa9cb04ccb1e34e9
Tom König <hi@tomknig.de>
2019-10-14 20:38:34 +0200

    Dark theme for the footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -14,11 +14,9 @@ import "typeface-source-sans-pro"
 
 import Header from "./header"
 import "../../static/styles/main.scss"
-import logo from "../../static/images/wbm.svg"
+import logo from "../../static/images/wbm--bright.svg"
 import iconTwitter from "../../static/images/icon-twitter.svg"
-import iconLinkedin from "../../static/images/icon-linkedin.svg"
 import iconGitHub from "../../static/images/icon-github.svg"
-import iconInstagram from "../../static/images/icon-instagram.svg"
 
 const Layout = ({children}) => (
     <StaticQuery query={graphql`
@@ -80,9 +78,7 @@ const Layout = ({children}) => (
                             <div className={"widget__item"}>
                                 <div className={"social"}>
                                     <a href="https://twitter.com/histaffio" target={"_blank"} title={"Twitter"}><img alt={"Twitter"} src={iconTwitter}/></a>
-                                    <a href="https://www.linkedin.com/company/histaff/" target={"_blank"} title={"LinkedIn"}><img alt={"LinkedIn"} src={iconLinkedin}/></a>
                                     <a href="https://github.com/histaff" target={"_blank"} title={"GitHub"}><img alt={"GitHub"} src={iconGitHub}/></a>
-                                    <a href="https://www.instagram.com/histaff.io/" target={"_blank"} title={"Instagram"}><img alt={"Instagram"} src={iconInstagram}/></a>
                                 </div>
                             </div>
                         </div>
