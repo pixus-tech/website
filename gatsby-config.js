dda855ad64e6eac5984753180bc006def888266f
Tom König <hi@tomknig.de>
2019-10-29 10:45:58 +0100

    Better scale of hero and new icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,11 +40,8 @@ module.exports = {
         background_color: `#f3f3f3`,
         theme_color: `#f3f3f3`,
         display: `minimal-ui`,
-        icon: `${__dirname}/src/images/icon.svg`, // This path is relative to the root of the site.
+        icon: `${__dirname}/src/images/app-icon-512.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
